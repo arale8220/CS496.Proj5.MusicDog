6c5fc26b5d23e9abfa66fb2393fb6f3c68440e79
LaLlorona <40677863+LaLlorona@users.noreply.github.com>
2018-07-30 20:00:55 +0900

    Squirrel + menu update
    1

---
+++ Assets/Scripts/MainmenuOption.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainmenuOption : MonoBehaviour {
+
+    static public bool[] isStageCleared = new bool[2];
+    Image map2Button;
+    
+
+    
+
+    private void Start()
+    {
+        
+        if (isStageCleared[0]&& SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            map2Button = GameObject.Find("LockImage").GetComponent<Image>();
+            map2Button.enabled = false;
+
+        }
+    }
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+
+
+    }
+    public void GoToMap1()
+    {
+        SceneManager.LoadScene(2);
+
+
+    }
+    public void GoToMap2()
+    {
+        if (isStageCleared[0])
+        {
+            SceneManager.LoadScene(3);
+        }
+        
+
+
+    }
+    public void GoToMapSelectAndEnableMap2()
+    {
+        isStageCleared[0] = true;
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
